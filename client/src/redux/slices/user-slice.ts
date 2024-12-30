a7ae8999d46e009ce5d292598ae4e4fedb67e1f3
riten <griten186@gmail.com>
2024-12-30 14:48:42 +0545

    feat: creating axios instance and set up redux store

---
+++ client/src/redux/slices/user-slice.ts
@@ -0,0 +1,58 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+import { getToken, getUserData, isUserLogin, resetLoginData, setUserLogin, setUserData } from "../../utils/auth-storage";
+
+export interface IUser{
+  id: number;
+  full_name: string;
+ 
+  email: string;
+  profile_pic: string;
+  created_at: string;
+  updated_at: string; 
+  role: string;
+  contact_info: string;
+  address: string;
+  userData?: {
+    email?: string;
+  };
+}
+
+export interface IUserState {
+  accessToken: string | null;
+  user: IUser | null;
+  loginStatus: boolean;
+}
+
+const initialState: IUserState = {
+  accessToken: getToken() || null,
+  user: getUserData() || {},
+  loginStatus: isUserLogin() || false,
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    setLogin: (state, action) => {
+      setUserLogin({ ...action.payload });
+      state.accessToken = action.payload?.token;
+      state.user = action.payload?.userData;
+      state.loginStatus = true;
+    },
+
+    setUser: (state, action) => {
+      setUserData({ ...state.user, ...action.payload });
+      state.user = { ...state.user, ...action.payload };
+    },
+
+    resetLogin: (state) => {
+      resetLoginData();
+      state.user = null;
+      state.loginStatus = false;
+    },
+  },
+});
+
+export const { setLogin, setUser, resetLogin } = userSlice.actions;
+export default userSlice.reducer;
---
+++ client/src/utils/auth-storage.ts
@@ -0,0 +1,71 @@
+import { IUser } from "../redux/slices/user-slice";
+
+interface ISetUserLoginProps {
+
+  userData: IUser;
+  token: string;
+}
+
+const TOKEN_KEY = "token";
+const USER_DATA_KEY = "user";
+
+export function getToken() {
+  const localStorageToken = localStorage.getItem(TOKEN_KEY);
+  const sessionStorageToken = sessionStorage.getItem(TOKEN_KEY);
+
+  if (localStorageToken) return localStorageToken;
+  if (sessionStorageToken) return sessionStorageToken;
+  return null;
+}
+
+
+export function setUserLogin(props: ISetUserLoginProps) {
+  const { userData, token } = props;
+
+
+
+  sessionStorage.setItem(USER_DATA_KEY, JSON.stringify(userData));
+  sessionStorage.setItem(TOKEN_KEY, token);
+}
+
+export function getUserData() {
+  const localStorageData = localStorage.getItem(USER_DATA_KEY);
+  const sessionStorageData = sessionStorage.getItem(USER_DATA_KEY);
+
+  try {
+    if (localStorageData) return JSON.parse(localStorageData);
+    if (sessionStorageData) return JSON.parse(sessionStorageData);
+  } catch {
+    return null;
+  }
+}
+
+export function isUserLogin() {
+  const localStorageToken = localStorage.getItem(TOKEN_KEY);
+  const sessionStorageToken = sessionStorage.getItem(TOKEN_KEY);
+  const userData = getUserData();
+
+  if (localStorageToken || sessionStorageToken || userData) return true;
+  return false;
+}
+
+
+export function resetLoginData() {
+  localStorage.clear();
+  sessionStorage.clear();
+}
+
+export function setUserData(userData: object) {
+  if (!userData) return;
+
+  const localStorageUserData = localStorage.getItem(USER_DATA_KEY);
+  const sessionStorageUserData = sessionStorage.getItem(USER_DATA_KEY);
+
+  if (localStorageUserData) {
+    localStorage.setItem(USER_DATA_KEY, JSON.stringify(userData));
+    return;
+  }
+  if (sessionStorageUserData) {
+    sessionStorage.setItem(USER_DATA_KEY, JSON.stringify(userData));
+  }
+}
